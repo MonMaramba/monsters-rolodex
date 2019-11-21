fea87f30158e9622a7e0d6127cbbcf74a7cbd857
unknown <monmaramba@yahoo.com>
2019-11-20 19:38:29 -0800

    Updates

---
+++ precache-manifest.637936c404172596c0dde5361be3b6e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3583b8aae889172ff9f79681785a24e0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "afe16b71a7075659d609",
+    "url": "/monsters-rolodex/static/css/main.1a7089f2.chunk.css"
+  },
+  {
+    "revision": "d75f0f40c1360681621f",
+    "url": "/monsters-rolodex/static/js/2.4f2343d6.chunk.js"
+  },
+  {
+    "revision": "afe16b71a7075659d609",
+    "url": "/monsters-rolodex/static/js/main.9ef34b4f.chunk.js"
+  },
+  {
+    "revision": "73b32e75c5111d7721fd",
+    "url": "/monsters-rolodex/static/js/runtime-main.6d40ac62.js"
+  }
+]);
